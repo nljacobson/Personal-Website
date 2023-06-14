80355d84526beb96ebd50e09adc23a6683b5ea13
nljacobson <nljacobs@uvm.edu>
2023-06-14 14:56:38 -0400

    Added require syntax for pdf-worker

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,20 +1,18 @@
 import { Document, Page, pdfjs } from 'react-pdf';
+import { Worker, Viewer } from '@react-pdf-viewer/core';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import './Resume.css';
 import resume from './HeaderlessResume.pdf';
 
-//Define worker for pdf reader package
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();
 
-export function Resume() {
-  console.log(resume)
-  return (
-    <Document height='1000px' className='resume-doc' file={resume}>
-      <Page pageNumber={1} />
-    </Document>
-  )
+export function Resume(){
+    return(
+            <Document height='1000px' className='resume-doc' file={resume}>
+                    <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}>
+                      <Viewer fileUrl={resume} />
+                    </Worker>
+                <Page pageNumber={1}/>
+            </Document>           
+    )
 }
