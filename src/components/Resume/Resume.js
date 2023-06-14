0687dd7cc584984c132adcc5719452c3e6c3da2a
nljacobson <nljacobs@uvm.edu>
2023-06-14 13:48:27 -0400

    Added print line to Resume.js

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -4,15 +4,17 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import './Resume.css';
 import resume from './HeaderlessResume.pdf';
 
+//Define worker for pdf reader package
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
   import.meta.url,
 ).toString();
 
-export function Resume(){
-    return(
-            <Document height='1000px' className='resume-doc' file={resume}>
-                <Page pageNumber={1}/>
-            </Document>           
-    )
+export function Resume() {
+  console.log(resume)
+  return (
+    <Document height='1000px' className='resume-doc' file={resume}>
+      <Page pageNumber={1} />
+    </Document>
+  )
 }
