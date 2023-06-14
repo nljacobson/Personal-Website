7bc27e691912dcd4784f0d610445cc89a6f8ae26
nljacobson <nljacobs@uvm.edu>
2023-06-14 15:52:01 -0400

    Corrected require syntax

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,5 +1,6 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 import { Document, Page, pdfjs } from 'react-pdf';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import './Resume.css';
@@ -9,9 +10,6 @@ import resume from './HeaderlessResume.pdf';
 export function Resume(){
     return(
             <Document height='1000px' className='resume-doc' file={resume}>
-                    <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}>
-                      <Viewer fileUrl={resume} />
-                    </Worker>
                 <Page pageNumber={1}/>
             </Document>           
     )
