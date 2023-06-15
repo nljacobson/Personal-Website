87928a4a7287d808f704940ba9d12f276d592860
nljacobson <nljacobs@uvm.edu>
2023-06-15 14:11:26 -0400

    Attempting to fix recursive html bug

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,12 +1,11 @@
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import './Resume.css';
-import resume from './HeaderlessResume.pdf';
 
 export function Resume(){
   return(
     <div>
-      <object type="application/pdf" data={resume} className='pdf-document'
+      <object type="application/pdf" data={'./HeaderlessResume.pdf'} className='pdf-document'
       style={{height: '100vh', width: '100vw'}}>
       <p>Failed to Load PDF!</p>
       </object>
