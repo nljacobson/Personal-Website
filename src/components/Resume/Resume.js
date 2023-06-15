7e467e581ca0a08489072c76c2afc9beaeeea9f6
nljacobson <nljacobs@uvm.edu>
2023-06-15 14:04:25 -0400

    Simplified Resume.js to use object type html component instead of react-pdf, which seemed buggy

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,15 +1,15 @@
-import {useEffect} from 'react';
-import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import './Resume.css';
 import resume from './HeaderlessResume.pdf';
 
 export function Resume(){
-  useEffect(() => { pdfjs.GlobalWorkerOptions.workerSrc =`https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;});
   return(
-          <Document height='1000px' className='resume-doc' file={resume}>
-              <Page pageNumber={1}/>
-          </Document>           
+    <div>
+      <object type="application/pdf" data={resume} className='pdf-document'
+      style={{height: '100vh', width: '100vw'}}>
+      <p>Failed to Load PDF!</p>
+      </object>
+    </div>      
   )
 }
