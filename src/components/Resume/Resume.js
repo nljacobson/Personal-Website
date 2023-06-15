c55c9cab14ae1c9f465007465aab78ebc2f451d1
nljacobson <nljacobs@uvm.edu>
2023-06-15 15:04:27 -0400

    Attempt 2 of the same process as last commit

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,10 +3,10 @@ import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import './Resume.css';
 
 export function Resume(){
-  const resume = './HeaderlessResume.pdf';
+  const resumeDocument = './HeaderlessResume.pdf';
   return(
     <div>
-      <object type="application/pdf" data={resume}
+      <object type="application/pdf" data={resumeDocument}
       style={{height: '100vh', width: '100vw'}}>
       <p>Failed to Load PDF!</p>
       </object>
