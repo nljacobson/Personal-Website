bba603cbca77b8b95c253737f9b0ad67b92a0c28
nljacobson <nljacobs@uvm.edu>
2023-06-16 11:48:07 -0400

    Removed Resume from build

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,10 +1,9 @@
 import './Resume.css';
-import resume from './HeaderlessResume.pdf'
 export function Resume(){
-  //const resumeDocument = process.env.PUBLIC_URL + "/HeaderlessResume.pdf";
+
   return(
-    <object data={resume} type="application/pdf" width="1000vh" height="1000vh">
-      <p>Unable to display PDF file. <a href={resume}>Download</a> instead.</p>
+    <object data={resumeDocument} type="application/pdf" width="1000vh" height="1000vh">
+      <p>Unable to display PDF file. <a href={resumeDocument}>Download</a> instead.</p>
     </object>  
   )
 }
