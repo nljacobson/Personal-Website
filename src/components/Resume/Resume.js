f3efb6809708427e09d428de7490bf2848312861
nljacobson <nljacobs@uvm.edu>
2023-06-16 12:14:08 -0400

    Added back resumeDocument

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,6 +1,6 @@
 import './Resume.css';
 export function Resume(){
-
+  const resumeDocument = "./text/HeaderlessResume.pdf"
   return(
     <object data={resumeDocument} type="application/pdf" width="1000vh" height="1000vh">
       <p>Unable to display PDF file. <a href={resumeDocument}>Download</a> instead.</p>
