0b693c2916d1aa388ed34c1f1214c3bc2f061179
nljacobson <nljacobs@uvm.edu>
2023-06-17 15:50:47 -0400

    used process.env.PUBLIC_URL for accessing public folder

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,9 +1,8 @@
 import './Resume.css';
-import resumeDocument from "./HeaderlessResume.pdf"
 export function Resume(){
   return(
-    <object data={resumeDocument} type="application/pdf" width="1000vh" height="1000vh">
-      <p>Unable to display PDF file. <a href={resumeDocument}>Download</a> instead.</p>
+    <object data={process.env.PUBLIC_URL + "/HeaderlessResume.pdf"} type="application/pdf" width="1000vh" height="1000vh">
+      <p>Unable to display PDF file. <a href={process.env.PUBLIC_URL + "/HeaderlessResume.pdf"}>Download</a> instead.</p>
     </object>  
   )
 }
