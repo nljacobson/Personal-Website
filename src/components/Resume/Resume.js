393f15036efb4682964bbcfc4eed589c884c4453
nljacobson <nljacobs@uvm.edu>
2023-06-17 18:26:18 -0400

    First attempt at modifying manifest.json

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,7 +1,7 @@
 import './Resume.css';
 export function Resume(){
   return(
-    <object data={process.env.PUBLIC_URL + "/HeaderlessResume.pdf"} type="application/pdf" width="1000vh" height="1000vh">
+    <object data={"./" + process.env.PUBLIC_URL + "/HeaderlessResume.pdf"} type="application/pdf" width="1000vh" height="1000vh">
       <p>Unable to display PDF file. <a href={process.env.PUBLIC_URL + "/HeaderlessResume.pdf"}>Download</a> instead.</p>
     </object>  
   )
