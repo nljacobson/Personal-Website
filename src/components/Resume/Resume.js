7f535f1d1108616c1475707d77830e08d51a6bab
nljacobson <nljacobs@uvm.edu>
2023-06-18 22:21:09 -0400

    Returned to %PUBLIC_URL% format

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,8 +1,8 @@
 import './Resume.css';
 export function Resume(){
   return(
-    <object data={"./" + process.env.PUBLIC_URL + "/HeaderlessResume.pdf"} type="application/pdf" width="1000vh" height="1000vh">
-      <p>Unable to display PDF file. <a href={process.env.PUBLIC_URL + "/HeaderlessResume.pdf"}>Download</a> instead.</p>
+    <object data={"./%PUBLIC_URL%/HeaderlessResume.pdf"} type="application/pdf" width="1000vh" height="1000vh">
+      <p>Unable to display PDF file. <a href={"./%PUBLIC_URL%/HeaderlessResume.pdf"}>Download</a> instead.</p>
     </object>  
   )
 }
